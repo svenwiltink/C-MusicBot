d67542bc77ff36aa15451db1fd7fa93d2210a9f3
swiltink <swiltink@transip.nl>
2020-01-14 21:19:06 +0100

    added a simple eventhandler func

--- mattermost/mattermost.h
+++ mattermost/mattermost.h
@@ -1,16 +1,33 @@
 #ifndef MATTERMOST_H_
-#define MATTERMOST_H_ 
+#define MATTERMOST_H_
 
-struct MatterMostApiOptions {
+struct MatterMostApiOptions
+{
     char *endpoint;
     char *token;
 };
 
-struct MatterMostUser {
+struct MatterMostUser
+{
     char *id;
     char *username;
     char *nickname;
 };
+
+struct MatterMostSession;
+
+struct MatterMostEvent
+{
+    char *data;
+};
+
+typedef void (*MatterMostHandleEvent)(struct MatterMostSession *session, struct MatterMostEvent event);
+
+typedef struct MatterMostSession
+{
+    char name[20];
+    MatterMostHandleEvent eventhandler;
+} MatterMostSession;
 
 int mattermost_get_user_self(struct MatterMostUser *user, struct MatterMostApiOptions options);
 
--- mattermost/mattermostsocket.c
+++ mattermost/mattermostsocket.c
@@ -8,92 +8,96 @@
 
 static struct lws *web_socket = NULL;
 
-struct MatterMostSession {
-	char name[20];
-};
+void DumpMessage(struct MatterMostSession *session, struct MatterMostEvent event)
+{
+	printf("session: %s\ndata: %s", session->name, event.data);
+}
 
-static int mattermost_callback( struct lws *wsi, enum lws_callback_reasons reason, void *user, void *in, size_t len )
+static int mattermost_callback(struct lws *wsi, enum lws_callback_reasons reason, void *user, void *in, size_t len)
 {
 
-	switch( reason )
+	switch (reason)
 	{
-		case LWS_CALLBACK_CLIENT_ESTABLISHED:
-            lws_callback_on_writable( web_socket );
-			break;
+	case LWS_CALLBACK_CLIENT_ESTABLISHED:
+		lws_callback_on_writable(web_socket);
+		break;
 
-		case LWS_CALLBACK_CLIENT_RECEIVE:
-            printf("RECEIVED DATA\n");
-            printf("%.*s\n", len, (char *) in);
-			/* Handle incomming messages here. */
-			break;
+	case LWS_CALLBACK_CLIENT_RECEIVE: ;
+		struct MatterMostEvent event = {
+			(char *) in
+		};
+		
+		struct MatterMostSession *session = (MatterMostSession *) user;
+		session->eventhandler(session, event);
+		break;
 
-		case LWS_CALLBACK_CLIENT_WRITEABLE:
-		{
-			struct MatterMostSession *sessionData = (struct MatterMostSession *) user;
-			printf(sessionData->name);
-            printf("writeable bitches!\n");
+	case LWS_CALLBACK_CLIENT_WRITEABLE:
+	{
+		struct MatterMostSession *sessionData = (struct MatterMostSession *)user;
+		printf(sessionData->name);
+		printf("writeable bitches!\n");
 
-            char *payload = "{ \"seq\": 1, \"action\": \"authentication_challenge\", \"data\": { \"token\": \"TOKEN\" } }";
-			unsigned char buf[LWS_SEND_BUFFER_PRE_PADDING + (sizeof(char) * strlen(payload) + 1) + LWS_SEND_BUFFER_POST_PADDING];
-			unsigned char *p = &buf[LWS_SEND_BUFFER_PRE_PADDING];
-			size_t n = sprintf( (char *)p, "%s", payload);
-			lws_write( wsi, p, n, LWS_WRITE_TEXT );
-			break;
-		}
+		char *payload = "{ \"seq\": 1, \"action\": \"authentication_challenge\", \"data\": { \"token\": \"TOKEN\" } }";
+		unsigned char buf[LWS_SEND_BUFFER_PRE_PADDING + (sizeof(char) * strlen(payload) + 1) + LWS_SEND_BUFFER_POST_PADDING];
+		unsigned char *p = &buf[LWS_SEND_BUFFER_PRE_PADDING];
+		size_t n = sprintf((char *)p, "%s", payload);
+		lws_write(wsi, p, n, LWS_WRITE_TEXT);
+		break;
+	}
 
-		case LWS_CALLBACK_CLOSED:
-		case LWS_CALLBACK_CLIENT_CONNECTION_ERROR:
-            printf("we DEAD\n");
-			web_socket = NULL;
-			break;
+	case LWS_CALLBACK_CLOSED:
+	case LWS_CALLBACK_CLIENT_CONNECTION_ERROR:
+		printf("we DEAD\n");
+		web_socket = NULL;
+		break;
 
-		default:
-            printf("got event %d\n", reason);
-			break;
+	default:
+		printf("got event %d\n", reason);
+		break;
 	}
 
 	return 0;
 }
 
 static struct lws_protocols protocols[] =
-{
 	{
-		"mattermost-protocol",
-		mattermost_callback,
-		sizeof(struct MatterMostSession),
-		0,
-	},
-	{ NULL, NULL, 0, 0 } /* terminator */
+		{
+			"mattermost-protocol",
+			mattermost_callback,
+			sizeof(struct MatterMostSession),
+			0,
+		},
+		{NULL, NULL, 0, 0} /* terminator */
 };
 
 void mattermost_connect(struct MatterMostApiOptions options)
 {
 	struct lws_context_creation_info info;
-	memset( &info, 0, sizeof(info) );
+	memset(&info, 0, sizeof(info));
 
 	info.port = CONTEXT_PORT_NO_LISTEN;
 	info.protocols = protocols;
 	info.gid = -1;
 	info.uid = -1;
-    info.options=LWS_SERVER_OPTION_DO_SSL_GLOBAL_INIT;
-    info.timeout_secs = 10;
+	info.options = LWS_SERVER_OPTION_DO_SSL_GLOBAL_INIT;
+	info.timeout_secs = 10;
 
-	struct lws_context *context = lws_create_context( &info );
+	struct lws_context *context = lws_create_context(&info);
 
 	struct MatterMostSession session = {
-		"lekkerman"
-	};
+		"lekkerman",
+		DumpMessage};
 
 	time_t old = 0;
-	while( 1 )
+	while (1)
 	{
 		struct timeval tv;
-		gettimeofday( &tv, NULL );
+		gettimeofday(&tv, NULL);
 
 		/* Connect if we are not connected to the server. */
-		if( !web_socket && tv.tv_sec != old )
+		if (!web_socket && tv.tv_sec != old)
 		{
-            printf("creating a new connection!\n");
+			printf("creating a new connection!\n");
 			struct lws_client_connect_info ccinfo = {0};
 			ccinfo.context = context;
 			ccinfo.address = "chat.transip.us";
@@ -103,19 +107,19 @@ void mattermost_connect(struct MatterMostApiOptions options)
 			ccinfo.origin = ccinfo.address;
 			ccinfo.protocol = protocols[0].name;
 			ccinfo.userdata = &session;
-            ccinfo.ssl_connection = (1 << 0) | (1 << 1) | (1 << 2) | (1 << 3) | (1 << 4);
+			ccinfo.ssl_connection = (1 << 0) | (1 << 1) | (1 << 2) | (1 << 3) | (1 << 4);
 			web_socket = lws_client_connect_via_info(&ccinfo);
 		}
 
-		if( tv.tv_sec != old )
+		if (tv.tv_sec != old)
 		{
 			/* Send a random number to the server every second. */
-			lws_callback_on_writable( web_socket );
+			lws_callback_on_writable(web_socket);
 			old = tv.tv_sec;
 		}
 
-		lws_service( context, /* timeout_ms = */ 250 );
+		lws_service(context, /* timeout_ms = */ 250);
 	}
 
-	lws_context_destroy( context );
+	lws_context_destroy(context);
 }
